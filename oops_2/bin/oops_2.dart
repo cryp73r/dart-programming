27e7fccc1e67d85d5bcebc830d2b4edc0d09e379
Priyanshu Singh <priyanshusingh0610@gmail.com>
2020-12-23 23:41:59 +0530

    Inheritance-Intermediate

--- oops_2/bin/oops_2.dart
+++ oops_2/bin/oops_2.dart
@@ -7,6 +7,10 @@ class Person {
   void showName() {
     print(this.name);
   }
+
+  void sayHello() {
+    print('Hello');
+  }
 }
 
 class Bonni extends Person {
@@ -17,23 +21,52 @@ class Bonni extends Person {
 
 class Priyanshu extends Person {
   bool PlayGames = true;
+
+  @override
+  void sayHello() {
+    // TODO: implement sayHello
+    print('Namaskar');
+  }
+}
+
+class Location {
+  num lat, lng;
+
+  Location(this.lat, this.lng);
+}
+
+class ElevatedLocation extends Location {
+  num elevation;
+  ElevatedLocation(num lat, num lng, this.elevation)
+      : super(lat, lng); // inheriting class with constructor
+
 }
 
 void main(List<String> arguments) {
   var bonni = new Bonni();
-  bonni.name = "Bonni";
+  bonni.name = 'Bonni';
   bonni.showName();
-  bonni.profession = "Architect";
+  bonni.profession = 'Architect';
   bonni.showProfession();
-  print("*************************");
+  bonni.sayHello();
+
+  print('*************************');
+
   var pr = new Priyanshu();
-  pr.name = "Priyanshu";
-  pr.lastName = "Singh";
+  pr.name = 'Priyanshu';
+  pr.lastName = 'Singh';
   pr.age = 20;
-  pr.nationality = "India";
+  pr.nationality = 'India';
   print(pr.name);
   print(pr.lastName);
   print(pr.age);
   print(pr.nationality);
   print(pr.PlayGames);
+  pr.sayHello();
+
+  print('*************************');
+
+  var elevated = new ElevatedLocation(23.89, -234.98, 90);
+
+  print('Location= ${elevated.lat}, ${elevated.lng}, ${elevated.elevation}');
 }
